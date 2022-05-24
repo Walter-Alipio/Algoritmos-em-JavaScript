28ee9803028e9403122ddddefb63871b7dcd9ff0
Walter <silvawalteralipio@gmail.com>
2022-05-24 10:30:49 -0300

    função que retorna a maior palavra de uma string

--- Desafios/longestWord.js
+++ Desafios/longestWord.js
@@ -2,18 +2,22 @@
   Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
 */
 
-const phrase = "Hello world123 567";
+const test1 = "Hello world123 567";
+const test2 = "fun&!! time_$#@ ";
 
 const LongestWord = (sen) => {
-  let count = 0;
+  const arr = sen
+    .split(" ")
+    .toString()
+    .match(/[a-zA-Z0-9]+/g); //Retorna um array com elementos que satisfazem a regex.
 
-  const arr = sen.split(" ");
+  /* abro o array a onde existem os acentos e filtro um novo array usando regex para tirar espaços vazios*/
+  //const arr = [...array.split(/[.,!;:?]/g)].filter((item) => /\S/.test(item));
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].length > arr[count].length) count = i;
-  }
+  arr.sort((a, b) => b.length - a.length);
 
-  return console.log(arr[count]);
+  return console.log(`The longest word is ${arr[0]}`);
 };
 
-LongestWord(phrase);
+LongestWord(test1);
+LongestWord(test2);
