1c6e3016053f5065bd127e2eb55ca7e8f7b3e4ad
Walter <silvawalteralipio@gmail.com>
2022-06-03 10:57:56 -0300

    refatorando

--- algoritmos-de-ordenacao/replace.js
+++
@@ -1,9 +0,0 @@
-function replace(list, higher, lower) {
-  let itemAnalysis = list[higher]; //guarda o conteúdo da posição atual
-  let itemPreviews = list[lower]; //guarda o conteúdo da posição que contem o menor preço
-
-  list[higher] = itemPreviews; //passa o conteúdo de menor preço para posição atual
-  list[lower] = itemAnalysis; //passa o conteúdo da posição atual para posição menor preço
-}
-
-module.exports = replace;
