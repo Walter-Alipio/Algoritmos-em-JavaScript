5c9272f67240003895adacaad9147e0c3071a540
Walter <silvawalteralipio@gmail.com>
2022-06-20 09:50:04 -0300

    plusMinus

--- Desafios-enterview/plusMinus.js
+++ Desafios-enterview/plusMinus.js
@@ -1,30 +1,4 @@
-"use strict";
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
-/*
- * Complete the 'plusMinus' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
+const arr = [1, 1, 0, -1, -1];
 
 function plusMinus(arr) {
   let negative = 0;
@@ -43,13 +17,4 @@ function plusMinus(arr) {
   });
 }
 
-function main() {
-  const n = parseInt(readLine().trim(), 10);
-
-  const arr = readLine()
-    .replace(/\s+$/g, "")
-    .split(" ")
-    .map((arrTemp) => parseInt(arrTemp, 10));
-  console.log(arr);
-  plusMinus(arr);
-}
+plusMinus(arr);
